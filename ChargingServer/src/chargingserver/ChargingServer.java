6b74e6dddff0399b95e449fd13d0760575f84554
Alex <openadbj@gmail.com>
2014-04-22 13:54:56 +0200

    Early days

---
+++ ChargingServer/src/chargingserver/ChargingServer.java
@@ -0,0 +1,31 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package chargingserver;
+
+/**
+ *
+ * @author Alex
+ */
+public class ChargingServer {
+String input = "$001L375ABK0Argentina*";
+    void methode(String input){
+        String dummy;
+        if (input.startsWith("*")){
+            dummy=input.substring(5, 5);
+            switch (dummy){
+                case "L" :  
+            }
+                    
+        }
+    }
+    
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
