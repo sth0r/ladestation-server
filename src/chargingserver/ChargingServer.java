7777c9bf1204af15238775260be98c89f94444e0
Thim <thimk9@gmail.com>
2014-05-20 13:21:03 +0200

    validate UID packet

--- src/chargingserver/ChargingServer.java
+++ src/chargingserver/ChargingServer.java
@@ -3,6 +3,7 @@
  * and open the template in the editor.
  */
 package chargingserver;
+
 import ihk.ibr.serial.*;
 import java.util.TooManyListenersException;
 import java.util.logging.Level;
@@ -13,41 +14,79 @@ import DAO.*;
  *
  * @author Alex
  */
+public class ChargingServer
+{
 
-
- public class ChargingServer {
     Transceiver tranciever;
 
-    public ChargingServer() {
-        try {
-            this.tranciever = new Transceiver("COM7",this);
-        } catch (TooManyListenersException ex) {
+    public ChargingServer()
+    {
+        try
+        {
+            this.tranciever = new Transceiver("COM7", this);
+        } catch (TooManyListenersException ex)
+        {
             Logger.getLogger(ChargingServer.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
 
-    public void Datareceived() {
+    public void Datareceived()
+    {
         String input = tranciever.getReceivedData();
         //System.out.println("CS31. DataReceived: " + input);
         commandInterp(input);
     }
-    
-    void commandInterp(String input){
-        System.out.println("CS36. CommandInterp: " + input);
-        if (input.startsWith("%") && input.endsWith("*")){
+    // validate card UID packet %001V7B503FD7*
+
+    void commandInterp(String input)
+    {
+        System.out.println("CS43. CommandInterp: " + input);
+        if (input.startsWith("%") && input.endsWith("*"))
+        {
             String command = input.substring(4, 5);
-            switch (command){
-                case "L" :  passcontrol(input); break;
-                case "C" :  chargecharge(input); break;
-                case "H" :  clockquery(input); break;
-                case "Q" :  pricequery(input); break;
+            switch (command)
+            {
+                case "V":
+                    validateUIDcontrol(input);
+                    break;
+                case "L":
+                    passcontrol(input);
+                    break;
+                case "C":
+                    chargecharge(input);
+                    break;
+                case "H":
+                    clockquery(input);
+                    break;
+                case "Q":
+                    pricequery(input);
+                    break;
             }
-                    
         }
     }
 
-    private void passcontrol(String input) {
-        int nextTAID=0;
+    private void validateUIDcontrol(String input)
+    {
+        String client = input.substring(1, 4);
+        String cardUID = input.substring(5, 13);
+        boolean validateUIDResult;                 //result = Control if password matches pass string
+        ChargingDAO chargingDAO = new ChargingDerbyDAO();
+        String customerUID = chargingDAO.findByUID(cardUID).getUID();
+        if (cardUID.equals(customerUID))
+        {
+            validateUIDResult = true;
+        } else
+        {
+            validateUIDResult = false;
+        }
+
+        validateUIDReturnPack(client, validateUIDResult);
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    private void passcontrol(String input)
+    {
+        int nextTAID = 0;
         String taID = String.valueOf(nextTAID);
         nextTAID++;
         String client = input.substring(1, 4);
@@ -59,17 +98,17 @@ import DAO.*;
         if (typedPassword.equals(customerPassword))
         {
             passResult = true;
-        }
-        else
+        } else
         {
             passResult = false;
         }
-        
-        loginReturnPack(client,taID,user,passResult);
+
+        loginReturnPack(client, taID, user, passResult);
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    private void chargecharge(String input) {
+    private void chargecharge(String input)
+    {
         String client = input.substring(1, 4);
         String taID = input.substring(5, 11);
         String user = input.substring(11, 19);
@@ -78,57 +117,73 @@ import DAO.*;
         String time = input.substring(25, 29);
         //træk kr/øre fra i brugerdatabase
         //put opladning i event database
-        
-        
+
+
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    private void clockquery(String input) {
+    private void clockquery(String input)
+    {
         String client = input.substring(1, 4);
         clockPack(client);
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    private void pricequery(String input) {
+    private void pricequery(String input)
+    {
         String client = input.substring(1, 4);
         pricePack(client);
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    public void clockBroadcast(int clientcount){
-        for (int i=1; i<=clientcount; i++){
+    public void clockBroadcast(int clientcount)
+    {
+        for (int i = 1; i <= clientcount; i++)
+        {
             clockPack(String.format("%03d", i));
         }
     }
-    
-    public void priceBroadcast(int clientcount){
-        for (int i=1; i<=clientcount; i++){
+
+    public void priceBroadcast(int clientcount)
+    {
+        for (int i = 1; i <= clientcount; i++)
+        {
             pricePack(String.format("%03d", i));
         }
     }
-    
-    private void loginReturnPack(String client, String taID, String user, boolean result) {
-        String pack='%'+client+"A"+taID+user+result;
+
+    private void loginReturnPack(String client, String taID, String user, boolean result)
+    {
+        String pack = '%' + client + "A" + taID + user + result;
         tranciever.transmit(pack);
-        System.out.println("CS113. Login return pack\n"+pack);
+        System.out.println("CS159. Login return pack\n" + pack);
+    }
+    
+    private void validateUIDReturnPack(String client, boolean result)
+    {
+        String pack = '%' + client + "A" + result;
+        tranciever.transmit(pack);
+        System.out.println("CS166. Validate UID return pack\n" + pack);
     }
 
-    private void pricePack(String client) {//"$"	"001"	"P"	New price kr	New price øre
-        String krData= "0000";
+    private void pricePack(String client)
+    {//"$"	"001"	"P"	New price kr	New price øre
+        String krData = "0000";
         String øreData = "00";
         //get prisdata from database
         double price = 0.55;//= new ChargingDerbyDAO().priceRequest(); // Gets price from database
-        String pack='%'+client+"P"+krData+øreData;
+        String pack = '%' + client + "P" + krData + øreData;
         tranciever.transmit(pack);
-        System.out.println("Price pack\n"+pack);
+        System.out.println("CS177. Price pack\n" + pack);
     }
-    
-    private void clockPack(String client) {//"$"	"001"	"T"	Hours	Minutes	Seconds
-        String hrData="0";
-        String minData="0";
-        String secData ="0";
+
+    private void clockPack(String client)
+    {//"$"	"001"	"T"	Hours	Minutes	Seconds
+        String hrData = "0";
+        String minData = "0";
+        String secData = "0";
         //get prisdata from database
-        String pack='%'+client+hrData+minData+secData;
+        String pack = '%' + client + hrData + minData + secData;
         tranciever.transmit(pack);
     }
 }
